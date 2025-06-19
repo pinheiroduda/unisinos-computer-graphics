79209db0411e6a4ba565f99e6c7e67668f68c74c
pinheiroduda <eduardapinheiro1302@gmail.com>
2025-06-19 20:44:06 -0300

    refactor: removes commented code

--- src/SuzanneTexture.cpp
+++ src/SuzanneTexture.cpp
@@ -54,28 +54,6 @@ string loadMTL(const string& path);
 // Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 1000, HEIGHT = 1000;
 
-// // Código fonte do Vertex Shader (em GLSL): ainda hardcoded
-// const GLchar* vertexShaderSource = "#version 450\n"
-// "layout (location = 0) in vec3 position;\n"
-// "layout (location = 1) in vec3 color;\n"
-// "uniform mat4 model;\n"
-// "out vec4 finalColor;\n"
-// "void main()\n"
-// "{\n"
-// //...pode ter mais linhas de código aqui!
-// "gl_Position = model * vec4(position, 1.0);\n"
-// "finalColor = vec4(color, 1.0);\n"
-// "}\0";
-
-// //Códifo fonte do Fragment Shader (em GLSL): ainda hardcoded
-// const GLchar* fragmentShaderSource = "#version 450\n"
-// "in vec4 finalColor;\n"
-// "out vec4 color;\n"
-// "void main()\n"
-// "{\n"
-// "color = finalColor;\n"
-// "}\n\0";
-
 bool rotateX=false, rotateY=false, rotateZ=false;
 
 glm::vec3 position = glm::vec3(0.2, 0.2, 0.2);
