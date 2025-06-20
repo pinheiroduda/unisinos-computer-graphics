bcf0f9d87140e65a9d4cc55635ddf82c4ac4040d
pinheiroduda <eduardapinheiro1302@gmail.com>
2025-06-20 18:34:48 -0300

    feat: creates lightned suzanne

---
+++ src/SuzannePhong.cpp
@@ -0,0 +1,520 @@
+/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
+ *
+ * Adaptado por Rossana Baptista Queiroz
+ * para as disciplinas de Processamento Gráfico/Computação Gráfica - Unisinos
+ * Versão inicial: 7/4/2017
+ * Última atualização em 07/03/2025
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <assert.h>
+
+using namespace std;
+
+// GLAD
+#include <glad/glad.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Shader.h>
+#include "stb_image.h"
+
+struct Light {
+    glm::vec3 ka;
+    glm::vec3 kd;
+    glm::vec3 ks;
+    glm::vec3 ke;
+    float q;
+};
+
+struct Geometry
+{
+    GLuint VAO;
+    GLuint vertexCount;
+    GLuint textureID = 0;
+    string textureFilePath;
+    Light light;
+};
+
+// Protótipo da função de callback de teclado
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Protótipos das funções
+int setupShader();
+Geometry setupGeometry(const char* filepath);
+
+bool loadObject(
+    const char* path,
+    std::vector<glm::vec3>& out_vertices,
+    std::vector<glm::vec2>& out_uvs,
+    std::vector<glm::vec3>& out_normals);
+int loadTexture(const string& path);
+Light loadMTL(const string& path, string& texturePath);
+
+// Dimensões da janela (pode ser alterado em tempo de execução)
+const GLuint WIDTH = 1000, HEIGHT = 1000;
+
+bool rotateX=false, rotateY=false, rotateZ=false;
+
+glm::vec3 position = glm::vec3(0.2, 0.2, 0.2);
+glm::vec3 scale = glm::vec3(0.4, 0.4, 0.4);
+
+glm::vec3 camPos(0.0f, 0.0f, 3.0f);
+glm::vec3 lightPos(2.0f, 2.0f, 1.0f);
+glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+
+string filePath = "";
+
+// Função MAIN
+int main()
+{
+	// Inicialização da GLFW
+	glfwInit();
+
+	// Criação da janela GLFW
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Suzanne Lightned", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	// Fazendo o registro da função de callback para a janela GLFW
+	glfwSetKeyCallback(window, key_callback);
+
+	// GLAD: carrega todos os ponteiros d funções da OpenGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+
+	}
+
+	// Obtendo as informações de versão
+	const GLubyte* renderer = glGetString(GL_RENDERER); /* get renderer string */
+	const GLubyte* version = glGetString(GL_VERSION); /* version as a string */
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
+
+	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+
+	Shader shader("../module4/shaders/vertex-shader.glsl", "../module4/shaders/fragment-shader.glsl");
+    Geometry geometry = setupGeometry("../assets/Modelos3D/SuzanneText.obj");
+
+	glUseProgram(shader.ID);
+
+	glm::mat4 model = glm::mat4(1); //matriz identidade;
+	GLint modelLoc = glGetUniformLocation(shader.ID, "model");
+
+    std::cout << "Ka: " << geometry.light.ka[0] << ", " << geometry.light.ka[1] << ", " << geometry.light.ka.b << std::endl;
+    std::cout << "Kd: " << geometry.light.kd[0] << ", " << geometry.light.kd[1] << ", " << geometry.light.kd.b << std::endl;
+    std::cout << "Ks: " << geometry.light.ks[0] << ", " << geometry.light.ks[1] << ", " << geometry.light.ks.b << std::endl;
+    std::cout << "Ns: " << geometry.light.q << std::endl;
+    std::cout << "lightPos: " << lightPos[0] << ", " << lightPos[1] << ", " << lightPos[2] << std::endl;
+
+    shader.setVec3("ka", geometry.light.ka.r, geometry.light.ka.g, geometry.light.ka.b);
+    shader.setVec3("kd", geometry.light.kd.r, geometry.light.kd.g, geometry.light.kd.b);
+    shader.setVec3("ks", geometry.light.ks.r, geometry.light.ks.g, geometry.light.ks.b);
+    shader.setVec3("ke", geometry.light.ke.r, geometry.light.ke.g, geometry.light.ke.b);
+    shader.setFloat("q", geometry.light.q);
+    
+    shader.setVec3("camPos", camPos[0], camPos[1], camPos[2]);
+    shader.setVec3("lightPos", lightPos[0], lightPos[1], lightPos[2]);
+    shader.setVec3("lightColor", lightColor[0], lightColor[1], lightColor[2]);
+
+    glm::mat4 view = glm::lookAt(camPos, glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+    shader.setMat4("view", value_ptr(view));
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+    shader.setMat4("projection", glm::value_ptr(projection));
+
+	model = glm::rotate(model, /*(GLfloat)glfwGetTime()*/glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	// Loop da aplicação - "game loop"
+	while (!glfwWindowShouldClose(window))
+	{
+		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funções de callback correspondentes
+		glfwPollEvents();
+
+		// Limpa o buffer de cor
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f); //cor de fundo
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glLineWidth(10);
+		glPointSize(20);
+
+		float angle = (GLfloat)glfwGetTime();
+
+        model = glm::mat4(1); 
+        model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        model = glm::translate(model, position);
+    
+        model = glm::scale(model, scale);
+
+        if (rotateX)
+        {
+            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+            
+        }
+        else if (rotateY)
+        {
+            model = glm::rotate(model, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+
+        }
+        else if (rotateZ)
+        {
+            model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+
+        }
+        
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, geometry.textureID);
+        glBindVertexArray(geometry.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, geometry.vertexCount);
+        
+        glBindVertexArray(0);
+        
+
+		// Troca os buffers da tela
+		glfwSwapBuffers(window);
+	}
+	// Pede pra OpenGL desalocar os buffers
+	glDeleteVertexArrays(1, &geometry.VAO);
+	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
+	glfwTerminate();
+	return 0;
+}
+
+// Função de callback de teclado - só pode ter uma instância (deve ser estática se
+// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
+// ou solta via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_X && action == GLFW_PRESS)
+	{
+		rotateX = true;
+		rotateY = false;
+		rotateZ = false;
+	}
+
+	if (key == GLFW_KEY_Y && action == GLFW_PRESS)
+	{
+		rotateX = false;
+		rotateY = true;
+		rotateZ = false;
+	}
+
+	if (key == GLFW_KEY_Z && action == GLFW_PRESS)
+	{
+		rotateX = false;
+		rotateY = false;
+		rotateZ = true;
+	}
+
+    if (key == GLFW_KEY_D && action == GLFW_PRESS)
+    {
+        position += glm::vec3(0.1, 0.0, 0.0);
+    }
+
+    if (key == GLFW_KEY_A && action == GLFW_PRESS)
+    {
+        position += glm::vec3(-0.1, 0.0, 0.0);
+    }
+
+    if (key == GLFW_KEY_W && action == GLFW_PRESS)
+    {
+        position += glm::vec3(0.0, 0.0, 0.1);
+    }
+
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
+    {
+        position += glm::vec3(0.0, 0.0, -0.1);
+    }
+
+    if (key == GLFW_KEY_I && action == GLFW_PRESS)
+    {
+        position += glm::vec3(0.0, 0.1, 0.0);
+    }
+
+    if (key == GLFW_KEY_J && action == GLFW_PRESS)
+    {
+        position += glm::vec3(0.0, -0.1, 0.0);
+    }
+
+    if (key == GLFW_KEY_H && action == GLFW_PRESS)
+    {
+        scale += glm::vec3(0.1, 0.1, 0.1);
+    }
+
+    if (key == GLFW_KEY_G && action == GLFW_PRESS)
+    {
+        scale += glm::vec3(-0.1, -0.1, -0.1);
+    }
+}
+
+
+// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a 
+// geometria de um triângulo
+// Apenas atributo coordenada nos vértices
+// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
+// A função retorna o identificador do VAO
+Geometry setupGeometry(const char* filepath)
+{
+    std::vector<GLfloat> vertices;
+	std::vector<glm::vec3> v;
+    std::vector<glm::vec2> uvs;
+    std::vector<glm::vec3> normals;
+
+    if (!loadObject(filepath, v, uvs, normals))
+    {
+        std::cerr << "Failed to load object geometry" << std::endl;
+        return {};
+    }
+
+    
+    vertices.reserve(v.size() * 8); // 3 posição + 3 cor + 2 UV
+    
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        vertices.insert(vertices.end(), {
+            v[i].x, v[i].y, v[i].z,     // posição
+             normals[i].x, normals[i].y, normals[i].z,   // normal      
+            uvs[i].x, uvs[i].y  // coordenadas de textura
+        });
+    }
+    
+    GLuint VAO, VBO;
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+	
+    // posição (location = 0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // normal (location = 1)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // UV (location = 2) 
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    Geometry geometry;
+    geometry.VAO = VAO;
+    geometry.vertexCount = vertices.size() / 8;
+
+	string basePath = string(filepath).substr(0, string(filepath).find_last_of("/"));
+    string filenameNoExt = string(filepath).substr(string(filepath).find_last_of("/") + 1);
+    filenameNoExt = filenameNoExt.substr(0, filenameNoExt.find_last_of(".")); // remove extensão
+
+    string mtlPath = basePath + "/" + filenameNoExt + ".mtl";
+    string textureFile;
+    geometry.light = loadMTL(mtlPath, textureFile);
+
+    if (!textureFile.empty())
+    {
+        string fullTexturePath = basePath + "/" + textureFile;
+        geometry.textureID = loadTexture(fullTexturePath);
+        geometry.textureFilePath = fullTexturePath;
+    }
+
+    return geometry;
+}
+
+bool loadObject(
+    const char* path,
+    std::vector<glm::vec3>& out_vertices,
+    std::vector<glm::vec2>& out_uvs,
+    std::vector<glm::vec3>& out_normals)
+{
+    std::ifstream file(path);
+
+    if (!file)
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        return false;
+    }
+
+    std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<glm::vec2> temp_uvs;
+    std::vector<glm::vec3> temp_normals;
+
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::string type;
+        iss >> type;
+
+        if (type == "v") // vertex data
+        {
+            glm::vec3 vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+            temp_vertices.push_back(vertex);
+        }
+        else if (type == "vt") // texture coordinate (UV)
+        {
+            glm::vec2 uv;
+            iss >> uv.x >> uv.y;
+            temp_uvs.push_back(uv);
+        }
+        else if (type == "vn") // normal vector
+        {
+            glm::vec3 normal;
+            iss >> normal.x >> normal.y >> normal.z;
+            temp_normals.push_back(normal);
+        }
+        else if (type == "f")
+        {
+            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            char slash;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                iss >> vertexIndex[i] >> slash >> uvIndex[i] >> slash >> normalIndex[i];
+                vertexIndices.push_back(vertexIndex[i]);
+                uvIndices.push_back(uvIndex[i]);
+                normalIndices.push_back(normalIndex[i]);
+            }
+        }
+    }
+
+    for (unsigned int i = 0; i < vertexIndices.size(); ++i)
+    {
+        unsigned int vertexIndex = vertexIndices[i];
+        unsigned int uvIndex = uvIndices[i];
+        unsigned int normalIndex = normalIndices[i];
+
+        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+        glm::vec2 uv = temp_uvs[uvIndex - 1];
+        glm::vec3 normal = temp_normals[normalIndex - 1];
+
+        out_vertices.push_back(vertex);
+        out_uvs.push_back(uv);
+        out_normals.push_back(normal);
+    }
+
+    file.close();
+
+    return true;
+}
+
+int loadTexture(const string& path)
+{
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        std::cout << "Loaded texture: " << path << " (" << width << "x" << height << ")" << std::endl;
+    }
+
+
+    if (data)
+    {
+        if (nrChannels == 3) //jpg, bmp
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        }
+        else //png
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        }
+            
+        glGenerateMipmap(GL_TEXTURE_2D); // geração do mipmap
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return texID;
+}
+
+Light loadMTL(const string& path, string& texturePath)
+{
+    ifstream mtlFile(path);
+    if (!mtlFile)
+    {
+        cerr << "Failed to open MTL file: " << path << endl;
+        return {};
+    }
+
+    Light light;
+    string line;
+    while (getline(mtlFile, line))
+    {
+        istringstream iss(line);
+        string keyword;
+        iss >> keyword;
+
+        if (keyword == "Ka") {
+            iss >> light.ka.r >> light.ka.g >> light.ka.b;
+        } else if (keyword == "Kd") {
+            iss >> light.kd.r >> light.kd.g >> light.kd.b;
+        } else if (keyword == "Ks") {
+            iss >> light.ks.r >> light.ks.g >> light.ks.b;
+        }  else if (keyword == "Ke")
+        {
+            iss >> light.ke.r >> light.ke.g >> light.ke.b;
+        } else if (keyword == "Ns") {
+            iss >> light.q;
+        } else if (keyword == "map_Kd") {
+            iss >> texturePath;
+        }
+    }
+
+    mtlFile.close();
+
+    if (texturePath.empty())
+    {
+        cerr << "No diffuse texture found in MTL file: " << path << endl;
+    }
+    return light;
+}
